ebc4b1b58520bd83209b1cdb4181204030181b3b
ARN <this.is.arn@yandex.ru>
2025-04-22 00:23:58 +0400

    create

---
+++ app/components/beauty-services-header/beauty-services-header.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Image from 'next/image';
+import './beauty-services-header.css';
+
+const BeautyServicesHeader: React.FC = () => {
+  return (
+    <div className="beauty-services-head">
+      <Image
+        src="/BeautyServices.png"
+        alt="BeautyServices"
+        width={479}
+        height={214}
+      />
+      <div className="beauty-services-head-text">
+        <p className="beauty-services-head-subtitle">
+          Luxury beauty services tailored just for you.<br />
+          Book your appointment today!
+        </p>
+        <button className="beauty-services-button-style">
+          <Image
+            src="/BeautyServicesStar.png"
+            alt="BeautyServicesStar"
+            width={32}
+            height={32}
+          />
+          eXPLORE all our SERVICES
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default BeautyServicesHeader;

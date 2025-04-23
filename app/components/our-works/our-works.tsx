dce7f20e87a9110c0874bca5157a7231e2e30cea
ARN <this.is.arn@yandex.ru>
2025-04-24 03:05:30 +0400

    create

---
+++ app/components/our-works/our-works.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import Image from 'next/image';
+import CardWrapper from '@/app/components/card-wrapper/card-wrapper';
+import { ourWorksMock } from '@/app/utils/mockFiles';
+import './our-works.css';
+import BeautyButton from '@/app/utils/ui/bs-button/bs-button';
+
+// interface BeautyServicesProps {
+//   type?: "page1" | "page2";
+// }
+
+const OurWorks: React.FC = () => {
+  return (
+    <section className={"our-works-section"} key={'our-works'}>
+      <BeautyButton
+        text="eXPLORE all our SERVICES"
+        className="beauty-services-button-style"
+      />
+      <div className={"our-works-info"}>
+        <Image
+          src={"/images/ow/ow-title.svg"}
+          alt={"title"}
+          width={684}
+          height={250}
+          className="ow-title"
+        />
+        <p className={"our-works-info-subtitle"}>
+          Experience our elegant beauty salon in the heart of London, where <br />
+          every corner is designed to inspire. Indulge in luxury treatments <br />
+          surrounded by a sophisticated interior, adorned with modern art pieces <br />
+          that elevate your beauty journey!
+        </p>
+      </div>
+      <ul className="our-works-images-list">
+        <li className="our-works-images-list-item ow-li-1">
+          <Image
+            src={ourWorksMock.image1}
+            alt={"image-1"}
+            width={300}
+            height={284}
+            className="our-works-image-1"
+          />
+        </li>
+        <li className="our-works-images-list-item ow-li-2">
+          <Image
+            src={ourWorksMock.image2}
+            alt={"image-2"}
+            width={300}
+            height={387}
+            className="our-works-image-2"
+          />
+        </li>
+        <li className="our-works-images-list-item ow-li-3">
+          <Image
+            src={ourWorksMock.image3}
+            alt={"image-3"}
+            width={286}
+            height={400}
+            className="our-works-image-3"
+          />
+        </li>
+        <li className="our-works-images-list-item ow-li-4">
+          <Image
+            src={ourWorksMock.image4}
+            alt={"image-4"}
+            width={440}
+            height={284}
+            className="our-works-image-4"
+          />
+        </li>
+        <li className="our-works-images-list-item ow-li-5">
+          <Image
+            src={ourWorksMock.image5}
+            alt={"image-5"}
+            width={916}
+            height={748}
+            className="our-works-image-5"
+          />
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default OurWorks;

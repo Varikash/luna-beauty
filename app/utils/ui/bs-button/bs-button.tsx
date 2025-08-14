e287b90fead7651676063589beb61bd921c32d83
ARN <this.is.arn@yandex.ru>
2025-08-14 06:52:58 +0400

    add variable routing

--- app/utils/ui/bs-button/bs-button.tsx
+++ app/utils/ui/bs-button/bs-button.tsx
@@ -1,17 +1,20 @@
 "use client"
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import './bs-button.css';
 
 interface BeautyButtonProps {
   text: string;
   onClick?: () => void;
+  link?: string;
   className?: string;
 }
 
 const BeautyButton: React.FC<BeautyButtonProps> = ({
   text,
   onClick,
+  link,
   className = ''
 }) => {
   const [isPressed, setIsPressed] = React.useState(false);
@@ -23,6 +26,26 @@ const BeautyButton: React.FC<BeautyButtonProps> = ({
   const handleMouseUp = () => {
     setIsPressed(false);
   };
+
+  if (link) {
+    return (
+      <Link
+        href={link}
+        className={`beauty-button ${isPressed ? 'pressed' : ''} ${className}`}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseLeave={() => setIsPressed(false)}
+      >
+        <Image
+          src={isPressed ? "/images/bs/beautyServicesStarWhite.svg" : "/images/bs/beautyServicesStar.png"}
+          alt="BeautyServicesStar"
+          width={32}
+          height={32}
+        />
+        {text}
+      </Link>
+    );
+  }
 
   return (
     <button

22ab45f52bdc895f2ee4f058c04fb268becb205a
ARN <this.is.arn@yandex.ru>
2025-08-29 00:56:28 +0400

    добавлены состояния компонента для разных страниц

--- app/components/beauty-services-header/beauty-services-header.tsx
+++ app/components/beauty-services-header/beauty-services-header.tsx
@@ -3,36 +3,47 @@ import Image from 'next/image';
 import './beauty-services-header.css';
 import BeautyButton from '@/app/utils/ui/bs-button/bs-button';
 
-const BeautyServicesHeader: React.FC = () => {
+const BeautyServicesHeader: React.FC<{ type?: string }> = ({ type }) => {
   return (
-    <div className="beauty-services-head">
-      <Image
-        src="/BeautyServices.png"
-        alt="BeautyServices"
-        width={479}
-        height={214}
-      />
-      <div className="beauty-services-head-text">
-        <p className="beauty-services-head-subtitle">
-          Luxury beauty services tailored just for you.<br />
-          Book your appointment today!
-        </p>
-        {/* <button className="beauty-services-button-style">
+    <>
+      {type != "page3" ? (
+        <div className="beauty-services-head">
           <Image
-            src="/BeautyServicesStar.png"
-            alt="BeautyServicesStar"
-            width={32}
-            height={32}
+            src="/BeautyServices.png"
+            alt="BeautyServices"
+            width={479}
+            height={214}
           />
-          eXPLORE all our SERVICES
-        </button> */}
-        <BeautyButton
-          text="eXPLORE all our SERVICES"
-          className="beauty-services-button-style"
-          link="/services"
-        />
-      </div>
-    </div>
+          <div className="beauty-services-head-text">
+            <p className="beauty-services-head-subtitle">
+              Luxury beauty services tailored just for you.<br />
+              Book your appointment today!
+            </p>
+            <BeautyButton
+              text="eXPLORE all our SERVICES"
+              className="beauty-services-button-style"
+              link="/services"
+            />
+          </div>
+        </div>
+      ) : (
+        <div className="beauty-services-head">
+          <Image
+            src="/BeautyServices.png"
+            alt="BeautyServices"
+            width={479}
+            height={214}
+          />
+          <div className="beauty-services-head-text-page3">
+            <p className="beauty-services-head-subtitle">
+              Luxury beauty services tailored<br /> just for you.<br />
+              Book your appointment today!<br />
+              High-quality services, from<br /> professional makeup and flawless<br /> waxing to the best experts in<br /> nails manicure!
+            </p>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 

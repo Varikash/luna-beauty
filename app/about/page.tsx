a63dff0d31d6f3554ea841b28fa99bacf8b17e28
ARN <this.is.arn@yandex.ru>
2025-11-13 01:06:22 +0400

    add footer

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,18 +6,20 @@ import Header from "../components/header/header";
 import OurMasters from "../components/our-masters/our-masters";
 import HeroAbout from "../components/HeroAbout/HeroAbout";
 import CooperComponent from "@/app/components/cooper-component/cooper-component";
+import Footer from "../components/footer/footer";
 
 export default function About() {
   return (
     <div className={styles.page}>
       <Header type="otherpages" />
       <main className={styles.main}>
-      {/* <HeroAbout /> */}
-      <CooperComponent />
+        <HeroAbout />
+        <CooperComponent />
         <OurMasters />
         <OurClientsRecommended />
         <BeautyServices type="page2" />
       </main>
+      <Footer />
     </div>
   );
 }

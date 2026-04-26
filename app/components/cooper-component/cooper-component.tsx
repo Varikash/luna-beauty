fb65fe276147986df2e8863940c89c28a690b451
Nicolai <nicolai.v@gbl-factory.org>
2026-04-26 22:20:41 +0300

    Refactor CooperComponent styles to enhance layout and responsiveness. Updated section structure and styles for better alignment and spacing, including adjustments to media queries for improved mobile display.

--- app/components/cooper-component/cooper-component.tsx
+++ app/components/cooper-component/cooper-component.tsx
@@ -6,40 +6,42 @@ export default function CooperComponent() {
 
   return (
     <section className={styles.cooper_section}>
-      <div className={styles.left_body}>
-        <div className={styles.images_wrapper}>
-          <Image
-            className={styles.image_woman}
-            src="/images/hero/hero-small-img.png"
-            alt="smiley woman"
-            width={228}
-            height={228}
-          />
-          <Image
-            src={`/images/footer/footerWhiteStar.svg`}
-            alt={"footer star"} width={85} height={85}
-            className={styles.image_star}
-          />
-        </div>
-        <p className={styles.cooper_info}>
-          At Luna we combine expertise, premium products,
-          and a relaxing atmosphere to ensure every
-          visit is a luxurious <br /> experience.</p>
-      </div>
-
-      <ul className={styles.right_body}>
-        {CooperGridContent.map((item, index) => (
-          <li className={`${styles.cooper_li} cooper_li_index_${index}`} key={`grid${item.title}`}>
+      <div className={styles.cooper_container}>
+        <div className={styles.left_body}>
+          <div className={styles.images_wrapper}>
             <Image
-              src={item.image}
-              alt={`${item.title} image`} width={85} height={64}
-              className={`${styles.images_right_body}`}
+              className={styles.image_woman}
+              src="/images/hero/hero-small-img.png"
+              alt="smiley woman"
+              width={228}
+              height={228}
             />
-            <h3 className={styles.right_li_title}>{item.title}</h3>
-            <p className={styles.right_li_subtitle}>{item.text}</p>
-          </li>
-        ))}
-      </ul>
+            <Image
+              src={`/images/footer/footerWhiteStar.svg`}
+              alt={"footer star"} width={85} height={85}
+              className={styles.image_star}
+            />
+          </div>
+          <p className={styles.cooper_info}>
+            At Luna we combine expertise, premium products,
+            and a relaxing atmosphere to ensure every
+            visit is a luxurious <br /> experience.</p>
+        </div>
+
+        <ul className={styles.right_body}>
+          {CooperGridContent.map((item, index) => (
+            <li className={`${styles.cooper_li} cooper_li_index_${index}`} key={`grid${item.title}`}>
+              <Image
+                src={item.image}
+                alt={`${item.title} image`} width={85} height={64}
+                className={`${styles.images_right_body}`}
+              />
+              <h3 className={styles.right_li_title}>{item.title}</h3>
+              <p className={styles.right_li_subtitle}>{item.text}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   )
 }
